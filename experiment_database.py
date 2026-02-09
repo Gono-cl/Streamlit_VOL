aeddcaec418b50a508156fe0cddf627cf945deab
Gonzalo Araya <gonzalo.arayav@gmail.com>
2026-02-09 12:26:17 +0100

    database remove user_email

--- experiment_database.py
+++ experiment_database.py
@@ -9,7 +9,7 @@ from sklearn.preprocessing import LabelEncoder
 st.title("📚 Experiment Database")
 st.markdown("### Experiment History")
 
-experiments = db_handler.list_experiments(st.user.email)
+experiments = db_handler.list_experiments()
 
 if not experiments:
     st.info("No experiments saved yet.")
